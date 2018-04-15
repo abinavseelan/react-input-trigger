ffe15ac075f626a9c68ec73d8191d1d457537928
Aditi Mohanty <rheaditi@users.noreply.github.com>
2018-04-15 20:34:11 +0530

    Fix webpack-dev-server

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -5,7 +5,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const commonConfiguration = require('./webpack.common.config');
 
-module.exports = merge(commonConfiguration, {
+module.exports = merge.strategy({ entry: 'replace' })(commonConfiguration, {
+  entry: { app: path.resolve(__dirname, 'src', 'docs', 'index.js'), },
   context: path.resolve(__dirname, 'src', 'docs'),
   devtool: 'cheap-eval-source-map', // transformed code (lines only),
   devServer: {
