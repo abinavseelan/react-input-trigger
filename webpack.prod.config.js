ce199bedf7415ac8832535cd82fe0cd49e80204d
Aditi Mohanty <rheaditi@users.noreply.github.com>
2019-06-05 22:22:47 +0530

    Use terser-plugin

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,47 +1,60 @@
 const webpack = require('webpack');
 const path = require('path');
 const merge = require('webpack-merge');
-const commonConfiguration = require('./webpack.common.config');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin');
 
-const outputPath = path.join(__dirname, 'build', 'lib');
-const filename = 'react-input-trigger';
-const libraryName = 'ReactInputTrigger';
-const production = JSON.stringify('production');
-const devtool = 'source-map'; // original source - for use in production
-
-const externals = { react: 'react' };
-const definePlugin = new webpack.DefinePlugin({ 'process.env.NODE_ENV': production });
-const uglifyPlugin = new UglifyJsPlugin();
-
-const libraryConfiguration = merge(commonConfiguration, {
-  devtool,
-  output: {
-    path: outputPath,
-    filename: '[name].js',
-    libraryTarget: 'commonjs2',
-  },
-  externals,
-  plugins: [uglifyPlugin, definePlugin],
-});
-
-const docsConfiguration = merge.strategy({ entry: 'replace' })(commonConfiguration, {
-  entry: { app: path.resolve(__dirname, 'src', 'docs', 'index.js'), },
+const commonConfiguration = require('./webpack.common.config');
+const productionConfiguration = {
   mode: 'production',
-  context: path.resolve(__dirname, 'src', 'docs'),
-  output: {
-    path: path.resolve(__dirname, 'docs'),
-    publicPath: './',
-    filename: '[name].[chunkhash:5].js',
+  devtool: 'source-map',
+  externals: { react: 'react' },
+  optimization: {
+    minimizer: [
+      new TerserPlugin()
+    ],
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: './index.html' }),
-    definePlugin,
-    uglifyPlugin,
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') })
   ],
-  devtool,
-});
+}
+
+const libraryConfiguration = merge(
+  commonConfiguration,
+  productionConfiguration,
+  {
+    output: {
+      path: path.join(__dirname, 'build', 'lib'),
+      filename: '[name].js',
+      libraryTarget: 'commonjs2',
+    },
+  }
+);
+
+const docsConfiguration = merge.strategy({ entry: 'replace' })(
+  commonConfiguration,
+  productionConfiguration,
+  {
+    entry: { app: path.resolve(__dirname, 'src', 'docs', 'index.js'), },
+    context: path.resolve(__dirname, 'src', 'docs'),
+    output: {
+      path: path.resolve(__dirname, 'docs'),
+      publicPath: './',
+      filename: '[name].[chunkhash:5].js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: './index.html' }),
+    ],
+  }
+);
+
+// for debugging merged options:
+// console.log(
+//   require('util').inspect({
+//     libraryConfiguration, docsConfiguration
+//   }, true, 4, true)
+// )
 
 module.exports = [
   libraryConfiguration,
