12913e00f6245ad7e4652152b1a3d9036aaeae41
Abinav Seelan <abinav.n.seelan@gmail.com>
2020-05-09 17:26:50 +0530

    Use typescript & React FC

--- src/index.tsx
+++ src/index.tsx
@@ -1,239 +1,56 @@
-import React, { Component, useEffect } from 'react';
+/* eslint-disable @typescript-eslint/no-empty-function */
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import getCaretCoordinates from 'textarea-caret';
 
-
-function getHookObject(type, element, startPoint) {
-  const caret = getCaretCoordinates(element, element.selectionEnd);
-
-  const result = {
-    hookType: type,
-    cursor: {
-      selectionStart: element.selectionStart,
-      selectionEnd: element.selectionEnd,
-      top: caret.top,
-      left: caret.left,
-      height: caret.height,
-    },
-  };
-
-  if (!startPoint) {
-    return result;
-  }
-
-  result.text = element.value.substr(startPoint, element.selectionStart);
-
-  return result;
-}
-
-// class InputTrigger extends Component {
-//   constructor(args) {
-//     super(args);
-
-//     this.state = {
-//       triggered: false,
-//       triggerStartPosition: null,
-//     };
-
-//     this.handleTrigger = this.handleTrigger.bind(this);
-//     this.resetState = this.resetState.bind(this);
-//     this.element = this.props.elementRef;
-//   }
-
-//   componentDidMount() {
-//     this.props.endTrigger(this.resetState);
-//   }
-
-//   handleTrigger(event) {
-//     const {
-//       trigger,
-//       onStart,
-//       onCancel,
-//       onType,
-//     } = this.props;
-
-//     const {
-//       which,
-//       shiftKey,
-//       metaKey,
-//       ctrlKey,
-//     } = event;
-
-//     const { selectionStart } = event.target;
-//     const { triggered, triggerStartPosition } = this.state;
-
-//     if (!triggered) {
-//       if (
-//         which === trigger.keyCode &&
-//         shiftKey === !!trigger.shiftKey &&
-//         ctrlKey === !!trigger.ctrlKey &&
-//         metaKey === !!trigger.metaKey
-//       ) {
-//         this.setState({
-//           triggered: true,
-//           triggerStartPosition: selectionStart + 1,
-//         }, () => {
-//           setTimeout(() => {
-//             onStart(getHookObject('start', this.element));
-//           }, 0);
-//         });
-//         return null;
-//       }
-//     } else {
-//       if (which === 8 && selectionStart <= triggerStartPosition) {
-//         this.setState({
-//           triggered: false,
-//           triggerStartPosition: null,
-//         }, () => {
-//           setTimeout(() => {
-//             onCancel(getHookObject('cancel', this.element));
-//           }, 0);
-//         });
-
-//         return null;
-//       }
-
-//       setTimeout(() => {
-//         onType(getHookObject('typing', this.element, triggerStartPosition));
-//       }, 0);
-//     }
-
-//     return null;
-//   }
-
-//   resetState() {
-//     this.setState({
-//       triggered: false,
-//       triggerStartPosition: null,
-//     });
-//   }
-
-//   render() {
-//     const {
-//       elementRef,
-//       children,
-//       trigger,
-//       onStart,
-//       onCancel,
-//       onType,
-//       endTrigger,
-//       ...rest
-//     } = this.props;
-
-//     return (
-//       <div
-//         role="textbox"
-//         tabIndex={-1}
-//         onKeyDown={this.handleTrigger}
-//         {...rest}
-//       >
-//         {
-//           !elementRef
-//             ? (
-//               React.Children.map(this.props.children, child => (
-//                 React.cloneElement(child, {
-//                   ref: (element) => {
-//                     this.element = element;
-//                     if (typeof child.ref === 'function') {
-//                       child.ref(element);
-//                     }
-//                   },
-//                 })
-//               ))
-//             )
-//             : (
-//               children
-//             )
-//         }
-//       </div>
-//     );
-//   }
-// }
-
-// InputTrigger.propTypes = {
-//   trigger: PropTypes.shape({
-//     keyCode: PropTypes.number,
-//     shiftKey: PropTypes.bool,
-//     ctrlKey: PropTypes.bool,
-//     metaKey: PropTypes.bool,
-//   }),
-//   onStart: PropTypes.func,
-//   onCancel: PropTypes.func,
-//   onType: PropTypes.func,
-//   endTrigger: PropTypes.func,
-//   children: PropTypes.element.isRequired,
-//   elementRef: PropTypes.element,
-// };
-
-// InputTrigger.defaultProps = {
-//   trigger: {
-//     keyCode: null,
-//     shiftKey: false,
-//     ctrlKey: false,
-//     metaKey: false,
-//   },
-//   onStart: () => {},
-//   onCancel: () => {},
-//   onType: () => {},
-//   endTrigger: () => {},
-//   elementRef: null,
-// };
-
-interface TriggerConfiguration {
-  id: string;
-  key: string;
-  shiftKey: string;
-  ctrlKey: string;
-  metaKey: string;
-}
-
-interface CursorMeta {
-  selectionStart: number;
-  selectionEnd: number;
+interface Caret {
   top: number;
   left: number;
   height: number;
 }
 
+interface TriggerConfiguration {
+  key: string;
+}
+
 interface TriggerStartEvent {
   hookType: 'start';
-  cursor: CursorMeta;
+  cursor: Caret;
 }
 
 interface TriggerKeyboardEvent {
   hookType: 'typing';
-  cursor: CursorMeta;
+  cursor: Caret;
   text: {
     value: string;
     content: string;
-  }
+  };
 }
 
 interface TriggerCancelEvent {
   hookType: 'cancel';
-  cursor: CursorMeta;
+  cursor: Caret;
   text: {
     value: string;
     content: string;
-  }
+  };
 }
 
 type TriggerEvent = TriggerStartEvent | TriggerKeyboardEvent | TriggerCancelEvent;
 
 interface InputTriggerProps {
   trigger: TriggerConfiguration;
-  onInputTrigger: (data: TriggerEvent) => void;
-  endTrigger: (callback: () => void) => void;
+  onInputTrigger?: (data: TriggerEvent) => void;
+  endTrigger?: (callback: () => void) => void;
 }
 
 
 const createTrigger = () => {
-  let triggered: boolean = false;
+  let triggered = false;
   let selectionStart: number | null = null;
 
   const startTrigger = (currentSelectionStart: number) => {
     triggered = true; selectionStart = currentSelectionStart;
-    console.log("startTrigger -> currentSelectionStart", currentSelectionStart);
   };
 
   const endTrigger = () => { triggered = false; selectionStart = null; };
@@ -254,7 +71,7 @@ const InputTrigger: React.FC<InputTriggerProps> = (props: React.PropsWithChildre
     }
   }, []);
 
-  const handleKeyPress = React.useCallback((event: React.KeyboardEvent<HTMLSpanElement>) => {
+  const handleKeyDown = React.useCallback((event: React.KeyboardEvent<HTMLSpanElement>) => {
     const {
       key,
     } = event;
@@ -264,36 +81,49 @@ const InputTrigger: React.FC<InputTriggerProps> = (props: React.PropsWithChildre
       value,
       selectionEnd,
     } = target;
-    const triggered = triggerRef.isTriggered();
 
-    if (!triggered && key === trigger.key) {
+    if (typeof selectionEnd === 'number') {
+      const triggered = triggerRef.isTriggered();
       const triggerObject = getCaretCoordinates(target, selectionEnd);
-      triggerRef.startTrigger(selectionEnd as number);
-      onInputTrigger({
-        hookType: 'start',
-        cursor: triggerObject,
-      });
-    } else if (triggered) {
-      const triggerObject = getCaretCoordinates(target, selectionEnd);
+      if (!triggered && key === trigger.key) {
+        triggerRef.startTrigger(selectionEnd as number);
+        if (typeof onInputTrigger === 'function') {
+          onInputTrigger({
+            hookType: 'start',
+            cursor: triggerObject,
+          });
+        }
+      } else if (triggered) {
+        if (typeof onInputTrigger === 'function') {
+          onInputTrigger({
+            hookType: 'typing',
+            cursor: triggerObject,
+            text: {
+              value: value.substring(triggerRef.getCurrentSelectionStart() as number),
+              content: value.substring(triggerRef.getCurrentSelectionStart() as number + 1),
+            },
+          });
+        }
 
-      onInputTrigger({
-        hookType: 'typing',
-        cursor: triggerObject,
-        text: {
-          value: value.substring(triggerRef.getCurrentSelectionStart() as number),
-          content: value.substring(triggerRef.getCurrentSelectionStart() as number + 1),
-        },
-      });
+      }
     }
   },[]);
 
   return (
     <span
-      onKeyPressCapture={handleKeyPress}
+      onKeyDownCapture={handleKeyDown}
     >
       {children}
     </span>
   );
+};
+
+InputTrigger.propTypes = {
+  trigger: PropTypes.shape({
+    key: PropTypes.string.isRequired,
+  }).isRequired,
+  onInputTrigger: PropTypes.func,
+  endTrigger: PropTypes.func,
 };
 
 export default InputTrigger;
