871f22480f0cf88d6cbfe9b296889566967a51e9
Abinav Seelan <abinav.n.seelan@gmail.com>
2020-05-09 14:50:32 +0530

    Typescript 💪

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -2,10 +2,21 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    'react-input-trigger': path.resolve(__dirname, 'src/index.js'),
+    'react-input-trigger': path.resolve(__dirname, 'src/index.tsx'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json', '.d.ts'],
+    alias: {
+      '@src': path.resolve('./src'),
+    },
   },
   module: {
     rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['babel-loader', 'awesome-typescript-loader'],
+        exclude: /node_modules/,
+      },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
