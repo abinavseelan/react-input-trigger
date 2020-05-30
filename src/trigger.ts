385732c81b2ae9685383ec1764b6959e21a73206
Aditi Mohanty <rheaditi@users.noreply.github.com>
2020-05-30 15:39:52 +0530

    fix: modifer assertion

--- src/trigger.ts
+++ src/trigger.ts
@@ -1,24 +1,35 @@
+import type React from 'react';
 import { TriggerConfiguration } from '@src/types';
 
 const compare = ['key', 'shiftKey', 'ctrlKey', 'metaKey', 'altKey'];
 
+const getTriggerConfiguration = (inputTrigger: TriggerConfiguration) => {
+  const trigger: TriggerConfiguration = {
+    shiftKey: undefined,
+    metaKey: undefined,
+    ctrlKey: undefined,
+    altKey: undefined,
+    ...inputTrigger,
+  }
+  return trigger;
+};
+
 export const createTrigger = (inputTrigger: TriggerConfiguration) => {
   let triggered = false;
   let selectionStart: number | null = null;
-  const trigger: TriggerConfiguration = {
-    shiftKey: false,
-    metaKey: false,
-    ctrlKey: false,
-    altKey: false,
-    ...inputTrigger,
-  }
+  const trigger: TriggerConfiguration = getTriggerConfiguration(inputTrigger);
 
   const startTrigger = (currentSelectionStart: number) => {
     triggered = true; selectionStart = currentSelectionStart;
   };
 
   const endTrigger = () => { triggered = false; selectionStart = null; };
-  const isStartOfTrigger = (event: React.KeyboardEvent) => compare.every(key => event[key] === trigger[key]);
+  const isStartOfTrigger = (event: React.KeyboardEvent) => compare.every(key => {
+    if (trigger[key] === undefined) {
+      return true;
+    }
+    return event[key] === trigger[key]
+  });
 
   const isTriggered = () => triggered === true;
   const getCurrentSelectionStart = () => selectionStart;
