41ea07f4e1faec037dc0748b49a890e9f59d81d9
Abinav Seelan <abinav.n.seelan@gmail.com>
2020-05-30 21:08:19 +0530

    try again

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,8 +1,8 @@
-const webpack = require('webpack');
 const path = require('path');
 const merge = require('webpack-merge');
 const commonConfiguration = require('./webpack.common.config');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 const filename = 'react-input-trigger';
 const libraryName = 'ReactInputTrigger';
@@ -10,28 +10,32 @@ const libraryName = 'ReactInputTrigger';
 const externals = { react: 'react' };
 
 const libraryConfiguration = merge(commonConfiguration, {
-  mode: 'production',
+  mode: "production",
   output: {
-    path: 'build/lib',
-    filename: '[name].js',
-    libraryTarget: 'commonjs2',
+    path: path.resolve(__dirname, "build/lib"),
+    filename: "[name].js",
+    libraryTarget: "commonjs2",
   },
   externals,
 });
+console.log("__dirname", __dirname)
 
-const docsConfiguration = merge.strategy({ entry: 'replace' })(commonConfiguration, {
-  mode: 'production',
-  entry: './src/docs/index.js'
-  output: {
-    path: './public',
-    publicPath: './',
-    filename: '[name].[chunkhash:5].js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: './index.html' }),
-    uglifyPlugin,
-  ],
-});
+const docsConfiguration = merge.strategy({ entry: "replace" })(
+  commonConfiguration,
+  {
+    mode: "production",
+    entry: "./src/docs/index.js",
+    output: {
+      path: path.resolve("./public"),
+      publicPath: "./",
+      filename: "[name].[chunkhash:5].js",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: "./src/docs/index.html" }),
+      new UglifyJsPlugin(),
+    ],
+  }
+);
 
 module.exports = [
   libraryConfiguration,
