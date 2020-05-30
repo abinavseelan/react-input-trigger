fc5c646cfe9619f8704fafa39638ddc489a7c9d8
Abinav Seelan <abinav.n.seelan@gmail.com>
2020-05-30 21:00:04 +0530

    remove ugligy

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,43 +3,34 @@ const path = require('path');
 const merge = require('webpack-merge');
 const commonConfiguration = require('./webpack.common.config');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
-const outputPath = path.join(__dirname, 'build', 'lib');
 const filename = 'react-input-trigger';
 const libraryName = 'ReactInputTrigger';
-const production = JSON.stringify('production');
-const devtool = 'source-map'; // original source - for use in production
 
 const externals = { react: 'react' };
-const definePlugin = new webpack.DefinePlugin({ 'process.env.NODE_ENV': production });
-const uglifyPlugin = new UglifyJsPlugin();
 
 const libraryConfiguration = merge(commonConfiguration, {
-  devtool,
+  mode: 'production',
   output: {
-    path: outputPath,
+    path: 'build/lib',
     filename: '[name].js',
     libraryTarget: 'commonjs2',
   },
   externals,
-  plugins: [uglifyPlugin, definePlugin],
 });
 
 const docsConfiguration = merge.strategy({ entry: 'replace' })(commonConfiguration, {
-  entry: { app: path.resolve(__dirname, 'src', 'docs', 'index.js'), },
-  context: path.resolve(__dirname, 'src', 'docs'),
+  mode: 'production',
+  entry: './src/docs/index.js'
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: './public',
     publicPath: './',
     filename: '[name].[chunkhash:5].js',
   },
   plugins: [
     new HtmlWebpackPlugin({ template: './index.html' }),
-    definePlugin,
     uglifyPlugin,
   ],
-  devtool,
 });
 
 module.exports = [
