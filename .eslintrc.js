319f586034184f44ff51df570e21c8a6335c646a
Aditi Mohanty <rheaditi@users.noreply.github.com>
2020-06-01 01:09:59 +0530

    fix: use package version from npm, add eslint & styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    'plugin:react-hooks/recommended',
   ],
   env: {
     browser: true,
