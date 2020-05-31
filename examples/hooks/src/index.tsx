95c3c980dee06f26acaebf6be878315693c49eaa
Aditi Mohanty <rheaditi@users.noreply.github.com>
2020-05-31 19:40:24 +0530

    wrap up examples

--- examples/hooks/src/index.tsx
+++ examples/hooks/src/index.tsx
@@ -1,3 +1,40 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import { useInputTrigger } from 'react-input-trigger';
 
+/** Using the hook `useInputTrigger()` in a function component. */
+const Example = () => {
+  const { triggerState, inputTriggerHandler, endTrigger } = useInputTrigger([
+    {
+      key: '@',
+      id: 'mention',
+    },
+    {
+      key: '/',
+      id: 'slash-command',
+    },
+  ]);
 
+  return (
+    <div>
+      <h1>react-input-trigger - useInputTrigger()</h1>
+      <p>
+        Start a trigger with {`'@'`} or {`'/'`}!
+      </p>
+      <textarea style={{ minWidth: '350px' }} rows={3} onKeyDown={inputTriggerHandler}/>
+      <div>
+        <div>
+          <button onClick={() => endTrigger()} disabled={!Boolean(triggerState)}>
+            End Trigger Manually
+          </button>
+        </div>
+        <br /><br />
+        <h3>trigger:</h3>
+        <pre>{triggerState ? JSON.stringify(triggerState, null, 2) : 'none'}</pre>
+      </div>
+    </div>
+  );
+};
 
+const rootElement = document.getElementById('root');
+render(<Example />, rootElement);
